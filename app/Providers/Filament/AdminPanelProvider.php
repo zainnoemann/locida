0d19d1bae7b8afc6dc0d77114d17ef65c295bdf4
zainnoemann <waznkuriere@gmail.com>
2026-06-04 00:19:51 +0800

    feat(test): remove global search from panel
    - disable Filament global search

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -26,6 +26,7 @@ class AdminPanelProvider extends PanelProvider
     {
         return $panel
             ->default()
+            ->globalSearch(false)
             ->id('admin')
             ->path('admin')
             ->viteTheme('resources/css/filament/admin/theme.css')
